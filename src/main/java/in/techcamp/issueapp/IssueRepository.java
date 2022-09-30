ed9a26a4b09a2ae171bf2e73cdeca44ac8bc9cd4
rikakohira <rikako.takase88@gmail.com>
2022-09-30 15:20:12 +0900

    リポジトリ　修正

--- src/main/java/in/techcamp/issueapp/IssueRepository.java
+++ src/main/java/in/techcamp/issueapp/IssueRepository.java
@@ -2,9 +2,6 @@ package in.techcamp.issueapp;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
 public interface IssueRepository {

51b069e2714ff656095a9414b2964041b7b05b56
rikakohira <rikako.takase88@gmail.com>
2022-09-30 15:19:23 +0900

    リポジトリを作成

---
+++ src/main/java/in/techcamp/issueapp/IssueRepository.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface IssueRepository {
+    @Insert("insert into issues (title, content, period, importance) values (#{title}, #{content}, #{period}, #{importance})")
+    void insert(String title, String content, String period, char importance);
+}

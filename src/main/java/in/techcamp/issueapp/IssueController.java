eab52fd1f0b9bd3ff23f8da87a93deb2cd6da7ec
rikakohira <rikako.takase88@gmail.com>
2022-09-30 18:33:22 +0900

    コントローラー修正

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,17 +1,25 @@
 package in.techcamp.issueapp;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@AllArgsConstructor
 public class IssueController {
+    private final IssueRepository issueRepository;
     //投稿機能
     @GetMapping("/issueForm")
     //IssueForm()メソッド
     public String showIssueForm(@ModelAttribute("issueForm") IssueForm form){
 
         return "issueForm";
+    }
+    @PostMapping("/issues")
+    public String createIssue(IssueForm form){
+        issueRepository.insert(form.getTitle(), form.getContent(), form.getPeriod(), form.getImportance());
+        return "redirect:/";
     }
 }
